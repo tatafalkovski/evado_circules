8d8ebc5fb38f521c2cf5c1d8369f42773ab9464f
tatafalkovski <falkovski.tata@gmail.com>
2020-12-17 16:45:39 +0200

    burger menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import "./main.scss";
+
+// Бургер
+var burger = document.querySelector('.burger-menu');
+var menu = document.querySelector('.menu__box');
+var burgerOff = false;
+function burgerHandler () {
+  if(burgerOff == false) {
+    menu.classList.add('open');
+    burgerOff = true;
+  } else {
+    menu.classList.remove('open');
+    burgerOff = false;
+  }
+}
+
+burger.onclick = burgerHandler;
